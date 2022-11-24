d2f7fd15974629bca3e03026348c541f3d5739c7
Niilo Kurki <niilo.kurki@helsinki.fi>
2022-11-24 19:58:26 +0200

    .

--- kurssi.py
+++ kurssi.py
@@ -1,12 +1,25 @@
+
+periodinkesto = 60
+
 class Kurssi:
-    def __init__(self):
-        self.nimi = ''
-        self.op = 0
-        self.laskarit = []
+    def __init__(self, json):
+        self.nimi = json.nimi
+        self.pisteet = 0
+        self.maksimipisteet: json.maksimipisteet
+        self.op = json.opintopisteet
+        self.palautukset = 
+        self.laskarit = [
+            {
+                "numero": 1,
+                "palautettu": False,
+                "pisteet": 10
+            }
+        ]
         self.luennot = []
         self.tentti = 0
 
     def get_arvosana():
         return 0
-
     arvosana = property(get_arvosana)
+
+
