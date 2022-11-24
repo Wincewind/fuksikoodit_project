13892503c7cc2624c85ae2d00fa8bebd3fef38da
Joni <joni.kaup@outlook.com>
2022-11-24 20:43:52 +0200

    .

--- opiskelija.py
+++ opiskelija.py
@@ -38,25 +38,4 @@ class Opiskelija:
                         luennot[luento].append(kurssi.nimi)
                     else:
                         luennot[luento]=[kurssi.nimi]
-
-                    
-
-
-
-    print(nimi)
-    print("Suoritat kursseja")
-    for kurssi in kurssit_meneillään:
-        print(kurssi)
-
-    print("Terveys: " + str(terveys) + "/100")
-    print("Motivaatio: " + str(motivaatio) + "/100")
-    print("Stressi: " + str(stressi))
-
-    print("Opintopisteitä kasassa: " + str(opintopisteet))
-    print("Olet suorittanut kurssit:")
-    for kurssi in suoritetut_kurssit:
-        print(kurssi)
-
-
-opiskelija = Opiskelija('matti')
-print(opiskelija)
+                        
