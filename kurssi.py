af9912248db3294f3c4f382a6b477dba1f6d67be
Tomi Ihalainen <tomi.ihalainen@helsinki.fi>
2022-11-24 18:58:57 +0200

    Kurssi-luokan dummy

---
+++ kurssi.py
@@ -0,0 +1,12 @@
+class Kurssi:
+    def __init__(self):
+        self.nimi = ''
+        self.op = 0
+        self.laskarit = []
+        self.luennot = []
+        self.tentti = 0
+
+    def get_arvosana():
+        return 0
+
+    arvosana = property(get_arvosana)
