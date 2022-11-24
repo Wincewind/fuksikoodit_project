42b4c4dcddf5dd745ab45457be40ab61daa940ff
Joni <joni.kaup@outlook.com>
2022-11-24 19:04:52 +0200

    tulostaa tiedot

--- opiskelija.py
+++ opiskelija.py
@@ -1,7 +1,23 @@
 class Opiskelija:
+
     nimi="nimi"
     opintopisteet=0
     suoritetut_kurssit=[]
+    kurssit_meneillään=[]
     terveys=100
     motivaatio=100
     stressi=0
+
+    print(nimi)
+    print("Suoritat kursseja")
+    for kurssi in kurssit_meneillään:
+        print(kurssi)
+
+    print("Terveys: " + str(terveys) + "/100")
+    print("Motivaatio: " + str(motivaatio) + "/100")
+    print("Stressi: " + str(stressi))
+
+    print("Opintopisteitä kasassa: " + str(opintopisteet))
+    print("Olet suorittanut kurssit:")
+    for kurssi in suoritetut_kurssit:
+        print(kurssi)
