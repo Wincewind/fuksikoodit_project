5fd3136cf384638b8bd1fecc7e729acf742bfbe0
Joni <joni.kaup@outlook.com>
2022-11-24 20:20:55 +0200

    .

--- opiskelija.py
+++ opiskelija.py
@@ -1,12 +1,46 @@
 class Opiskelija:
 
-    nimi="nimi"
-    opintopisteet=0
-    suoritetut_kurssit=[]
-    kurssit_meneillään=[]
-    terveys=100
-    motivaatio=100
-    stressi=0
+
+    def __init__(self, nimi):
+        self.nimi = nimi
+        self.opintopisteet = 0
+        self.suoritetut_kurssit = []
+        self.kurssit_meneillään = []
+        self.terveys = 100
+        self.motivaatio = 100
+        self.stressi = 0
+
+    def __str__(self):
+        return f"Terveys: {self.terveys}/100\nMotivaatio: {self.motivaatio}/100\nStressi: {self.stressi}"
+        
+    def opintopiste_tulostus(self):
+        return "Opintopisteitä kasassa: " + str(self.opintopisteet)
+
+    def suoritetut_kurssit_tulostus(self):
+        suoritetut=""
+        for kurssi in self.suoritetut_kurssit:
+            suoritetut+=kurssi+"\n"
+        return suoritetut
+
+
+    def kurssit_meneillään_tulostus(self):
+        meneillään=""
+        for kurssi in self.kurssit_meneillään:
+            meneillään+=kurssi+"\n"
+        return meneillään
+
+    def luennot_väliltä(self, alku, loppu):
+        luennot={}
+        luentopäivät={}
+        for kurssi in self.kurssit_meneillään:
+            for luento in kurssi.luennot:
+                if alku<=luento<=loppu:
+                    luentopäivät.update(luento:luentopäivät[luento]+kurssi.nimi)
+                    luennot[luento]=kurssi.nimi
+
+                    
+
+
 
     print(nimi)
     print("Suoritat kursseja")
@@ -21,3 +55,7 @@ class Opiskelija:
     print("Olet suorittanut kurssit:")
     for kurssi in suoritetut_kurssit:
         print(kurssi)
+
+
+opiskelija = Opiskelija('matti')
+print(opiskelija)
